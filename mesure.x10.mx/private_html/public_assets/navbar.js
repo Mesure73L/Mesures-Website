9bb63ba685a9fe1315478dfff8ad42f4881460cd
Mesure73L <115181664+Mesure73L@users.noreply.github.com>
2024-02-07 19:02:40 +0000

    Made navbar use relative links instead of direct links (#121)

--- mesure.x10.mx/private_html/public_assets/navbar.js
+++ mesure.x10.mx/private_html/public_assets/navbar.js
@@ -1,17 +1,19 @@
-fetch('https://mesure.x10.mx/public_assets/navbar.html')
-    .then(res => res.text())
-    .then(text => {
-        let oldelem = document.querySelector("script#replace_with_navbar");
-        let newelem = document.createElement("div");
-        newelem.innerHTML = text;
-        oldelem.parentNode.replaceChild(newelem, oldelem);
-    })
+function getNavbar(nav, mobile) {
+    fetch(`${nav}/navbar.html`)
+        .then(res => res.text())
+        .then(text => {
+            let oldelem = document.querySelector("script#replace_with_navbar");
+            let newelem = document.createElement("div");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem, oldelem);
+        })
 
-fetch('https://mesure.x10.mx/public_assets/mobile_navbar.js')
-    .then(res => res.text())
-    .then(text => {
-        let oldelem = document.querySelector("script#mobile_navbar");
-        let newelem = document.createElement("script");
-        newelem.innerHTML = text;
-        oldelem.parentNode.replaceChild(newelem, oldelem);
-    })
+    fetch(`${mobile}/mobile_navbar.js`)
+        .then(res => res.text())
+        .then(text => {
+            let oldelem = document.querySelector("script#mobile_navbar");
+            let newelem = document.createElement("script");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem, oldelem);
+        })
+}
