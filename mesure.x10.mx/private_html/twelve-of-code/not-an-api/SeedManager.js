de051a2ea9e8b1158fb70b08656600df65eb3631
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2024-02-27 13:46:50 -0600

    Removed comment

--- mesure.x10.mx/private_html/twelve-of-code/not-an-api/SeedManager.js
+++ mesure.x10.mx/private_html/twelve-of-code/not-an-api/SeedManager.js
@@ -1,5 +1,5 @@
 function RNG(seed) {
-  this.m = 0x80000000; // 2**31;
+  this.m = 0x80000000;
   this.a = 1103515245;
   this.c = 12345;
   this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
